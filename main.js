0d8c47ab40d67e349d96931eb6e6ce2ba3d11133
Gbissell93 <gregorybisselledu@gmail.com>
2021-07-19 12:40:53 -0400

    all DOM changes complete

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const crossout = document.querySelectorAll('.crossout');
 crossout.forEach(el => el.addEventListener('click', () => {
-    if (el.style.textDecoration === '') {
     el.style.textDecoration = 'line-through'
-    }
-
-    else {
-        crossout.style.textDecoration = ''
-    }
 }))
+
+const poof = document.querySelectorAll('.poof');
+poof.forEach(el => el.addEventListener('click', () => { 
+    el.style.opacity = '0'
+}))
+
+const bye = document.querySelectorAll('.bye');
+bye.forEach(el => el.addEventListener('click', () => { 
+    el.style.width = '0'
+}))
+
+const destroy = document.querySelector('#destroy-all');
+destroy.addEventListener('click', () => {
+    crossout.forEach(el => el.style.textDecoration = 'line-through')
+    poof.forEach(el => el.style.opacity = '0')
+    bye.forEach(el => el.style.width = '0')
+    
+    
+})
